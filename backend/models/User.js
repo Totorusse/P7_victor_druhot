c8796a5437b6e341fc26db269175b349b95a1c2d
Totorusse <victordruhot@gmail.com>
2022-06-02 12:16:48 +0200

    ADD slots to db

--- backend/models/User.js
+++ backend/models/User.js
@@ -30,6 +30,42 @@ module.exports = (sequelize, Sequelize) => {
     Slot4: {
       type: Sequelize.STRING,
     },
+    Slot5: {
+      type: Sequelize.STRING,
+    },
+    Slot6: {
+      type: Sequelize.STRING,
+    },
+    Slot7: {
+      type: Sequelize.STRING,
+    },
+    Slot8: {
+      type: Sequelize.STRING,
+    },
+    Slot9: {
+      type: Sequelize.STRING,
+    },
+    Slot10: {
+      type: Sequelize.STRING,
+    },
+    Slot11: {
+      type: Sequelize.STRING,
+    },
+    Slot12: {
+      type: Sequelize.STRING,
+    },
+    Slot13: {
+      type: Sequelize.STRING,
+    },
+    Slot14: {
+      type: Sequelize.STRING,
+    },
+    Slot15: {
+      type: Sequelize.STRING,
+    },
+    Slot16: {
+      type: Sequelize.STRING,
+    },
   });
   return User;
 };
